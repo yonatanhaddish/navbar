ef254c38c4a77ded0945912ba19b63626f5ea631
Yonatan Haddish <yonihailshn@gmail.com>
2022-08-09 19:18:56 -0400

    some styling on the navlogo navbar01

--- src/components/Navbar01/Navbar01.js
+++ src/components/Navbar01/Navbar01.js
@@ -7,7 +7,7 @@ const Navbar01 = () => {
     <div className='navbar01'>
         <div className='navbar-container'>
             <div className='navbar-logo'>
-                <div><a href= "/">Logo</a></div>
+                <div><a className='name-model' href= "/">FirstName LastName</a></div>
             </div>
                 <div className='list-items'>
                     {/* <li className='list'><a href= "#">Home</a></li>
@@ -15,9 +15,9 @@ const Navbar01 = () => {
                     <li className='list'><a href= "#">Pricing</a></li>
                     <li className='list'><a href= "#">About</a></li> */}
                     <button><a href= "/">Home</a></button>
-                    <button><a href= "/feature">Feature</a></button>
-                    <button><a href= "/pricing">Pricing</a></button>
+                    <button><a href= "/portfolio">Portfolio</a></button>
                     <button><a href= "/about">About</a></button>
+                    <button><a href= "/contact">Contact</a></button>
                 </div>
         </div>
     </div>
